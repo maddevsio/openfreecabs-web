094c3c1c262e47eb9b9a3a45ecb8568bd8a47736
M1rotvorez <vokik91@gmail.com>
2016-10-06 14:50:14 +0600

    refactor js with CL / add styles for buttons

--- static/app.js
+++ static/app.js
@@ -2,7 +2,6 @@ var map;
 var markers = [];
 var currentLocation;
 var defaultCoords = {lat: 42.876680, lng: 74.588665};
-var geoloccontrol = new klokantech.GeolocationControl(map, mapMaxZoom);
 
 function initMap() {
 		var locIcon = '/static/loc.png';
@@ -53,6 +52,87 @@ function changeLocationPin(latlng) {
 		requestNearest(latlng.lat(), latlng.lng());
 }
 
+function fillCompanyContact(company) {
+		var filledContact = {
+				phoneNumber: "n/a",
+				shortNumber: "",
+				webSite: "#",
+				android: "#",
+				icon: "static/img/icon_namba.png",
+				iOS: "#"
+		};
+
+		if (company.contacts) {
+				var contacts = company.contacts;
+
+				for (var l = 0, lenC=contacts.length; l < lenC; l++) {
+						var contactData = company.contacts[l];
+						// console.log(c);
+						filledContact = fillContact(filledContact, contactData);
+				}
+		}
+		return filledContact;
+}
+
+function fillContact(contact, contactData){
+		switch (contactData.type) {
+				case 'phone':
+						contact.phoneNumber = contactData.contact;
+						break;
+				case 'sms':
+						contact.shortNumber = contactData.contact;
+						break;
+				case 'android':
+						contact.android = contactData.contact;
+						break;
+				case 'ios':
+						contact.iOS = contactData.contact;
+						break;
+				case 'website':
+						contact.webSite = contactData.contact;
+						break;
+				case 'icon':
+						contact.icon = contactData.icon;
+						break;
+		}
+		return contact;
+}
+
+function renderDriversSidebar(company, drivers, contact){
+		var source = document.getElementById('result').innerHTML;
+		var template = Handlebars.compile(source);
+		var html = template({
+				name: company.name,
+				icon: company.icon,
+				driversCount: drivers.length,
+				phoneNumber: contact.phoneNumber,
+				shortNumber: contact.shortNumber,
+				webSite: contact.webSite,
+				android: contact.android,
+				iOS: contact.iOS
+		});
+		return html;
+}
+
+function renderDriverMarkers(drivers, company){
+		var driverMarkers = [];
+		for (var j = 0; j < drivers.length; j++) {
+				var item = drivers[j];
+				var options = {
+						position: {lat: item.lat, lng: item.lon},
+						map: map,
+						title: company.name
+				};
+				if (company.icon !== "" && company.icon !== window.location.origin) {
+						options.icon = company.icon
+				}
+				driverMarkers.push(
+						new google.maps.Marker(options)
+				)
+		}
+		return driverMarkers;
+}
+
 function requestNearest(lat, lng) {
 		// var url = window.location.origin + "/nearest/" + lat + "/" + lng;
 
@@ -63,79 +143,32 @@ function requestNearest(lat, lng) {
 		xmlhttp = new XMLHttpRequest();
 		xmlhttp.onreadystatechange = function () {
 				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-						data = JSON.parse(xmlhttp.responseText);
-						console.log(data);
+						var data = JSON.parse(xmlhttp.responseText);
+						// console.log(data);
 						if (data.success) {
-								cleanMarkers();
 								data.companies = data.companies.sort(function (a, b) {
 										return a.drivers.length < b.drivers.length
 								});
 
-
-								for (i = 0; i < data.companies.length; i++) {
+								cleanMarkers();
+								for (var i = 0, len=data.companies.length; i < len; i++) {
 
 										var company = data.companies[i];
-										var phoneNumber = "n/a",
-												shortNumber = "",
-												webSite = "#",
-												android = "#",
-												icon = "static/img/icon_namba.png",
-												iOS = "#";
-										if (company.contacts !== null) {
-												for (l = 0; l < company.contacts.length; l++) {
-														c = company.contacts[l];
-														console.log(c);
-														switch (c.type) {
-																case 'phone':
-																		phoneNumber = c.contact;
-																		break;
-																case 'sms':
-																		shortNumber = c.contact;
-																		break;
-																case 'android':
-																		android = c.contact;
-																		break;
-																case 'ios':
-																		iOS = c.contact;
-																		break;
-																case 'website':
-																		webSite = c.contact;
-																		break;
-																case 'icon':
-																		icon = c.icon;
-																		break;
-														}
-												}
-										}
-										console.log(phoneNumber);
-										console.log(shortNumber);
-										var source = document.getElementById('result').innerHTML;
-										var template = Handlebars.compile(source);
-										var html = template({
-												name: company.name,
-												icon: company.icon,
-												driversCount: company.drivers.length,
-												phoneNumber: phoneNumber,
-												shortNumber: shortNumber,
-												webSite: webSite,
-												android: android,
-												iOS: iOS
+										var drivers = company.drivers;
+                    var contact = fillCompanyContact(company);
+										var $resultWrap = $(".result_wrap");
+
+										$resultWrap.find('.result_title-phone').off('click');
+
+										var driverSidebarHtml = renderDriversSidebar(company, drivers, contact);
+
+										$resultWrap.append(driverSidebarHtml);
+
+										markers = markers.concat(renderDriverMarkers(drivers, company));
+										$resultWrap.find('.result_title-phone').click(function(){
+												var phone = $(this).data("phone");
+												$(this).val(phone);
 										});
-										$(".result_wrap").append(html);
-										for (j = 0; j < company.drivers.length; j++) {
-												var item = company.drivers[j];
-												var options = {
-														position: {lat: item.lat, lng: item.lon},
-														map: map,
-														title: company.name
-												};
-												if (company.icon !== "" && company.icon !== window.location.origin) {
-														options.icon = company.icon
-												}
-												markers.push(
-														new google.maps.Marker(options)
-												)
-										}
 								}
 						}
 				}
@@ -145,7 +178,8 @@ function requestNearest(lat, lng) {
 }
 
 function cleanMarkers() {
-		for (i = 0; i < markers.length; i++) {
+		console.log("Markers: ", markers.length);
+		for (var i = 0; i < markers.length; i++) {
 				var marker = markers[i];
 				marker.setMap(null);
 		}
